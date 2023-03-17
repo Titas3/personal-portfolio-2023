e918fe0db5d7d90e7881b7f790081eb3a52aaf25
Titas Chowdhury <titaschowdhury3gmail.com>
2023-03-17 15:52:17 +0530

    commit

---
+++ src/data.js
@@ -0,0 +1,81 @@
+import p1 from "./assets/project-1.png";
+import p2 from "./assets/project-2.png";
+import p3 from "./assets/project-3.png";
+import p4 from "./assets/project-4.png";
+import p5 from "./assets/project-5.png";
+import p6 from "./assets/project-6.png";
+
+export const filters = [
+    {
+        name: "All",
+        id: "all",
+    },
+    {
+        name: "Dashboard",
+        id: "dashboard",
+    },
+    {
+        name: "React App",
+        id: "React-app",
+    },
+    {
+        name: "Website",
+        id: "website",
+    },
+];
+
+export const projects = [
+    {
+        name: "Netflix Clone",
+        image: p1,
+        tags: ["dashboard", "React-app"],
+    },
+    {
+        name: "Gym Register Page Website",
+        image: p2,
+        tags: ["dashboard", "website"],
+    },
+    {
+        name: "E-Commerce website Tailwind",
+        image: p3,
+        tags: ["React-app"],
+    },
+    {
+        name: "React To Do List",
+        image: p4,
+        tags: ["React-app"],
+    },
+    {
+        name: "The Playstation Website",
+        image: p5,
+        tags: ["website", "dashboard"],
+    },
+    {
+        name: "Flowers Shop",
+        image: p6,
+        tags: ["website"],
+    },
+];
+
+export const achievements = [
+    {
+        id: 1,
+        year: 40,
+        description: "SUCCESSFULLY<br />COMPLETED<br />PROJECTS",
+    },
+    {
+        id: 2,
+        year: 1.5,
+        description: "YEARS OF<br />EXPERIENCE",
+    },
+    {
+        id: 3,
+        year: 20,
+        description: "TECHNOLOGIES",
+    },
+    {
+        id: 4,
+        year: 30,
+        description: "COMMUNITY<br />POSTS",
+    },
+];
